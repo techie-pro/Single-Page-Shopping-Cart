b4d9d6f7877255c6ef5ad40a2526327bc237c93d
techie-pro <techiepro6@gmail.com>
2022-08-11 12:46:40 +0530

    Implemented Store Page UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,17 +3,17 @@ import { NavLink } from 'react-router-dom';
 
 export function Navbar() {
   return (
-    <Navbarbs className='bg-white shadow-sm mb-3'>
+    <Navbarbs sticky='top' className='bg-white shadow-sm mb-3'>
       <Container>
         <Nav className='me-auto'>
           <Nav.Link to='/' as={NavLink}>
             Home
           </Nav.Link>
-          <Nav.Link to='/about' as={NavLink}>
-            About
-          </Nav.Link>
           <Nav.Link to='/store' as={NavLink}>
             Store
+          </Nav.Link>
+          <Nav.Link to='/about' as={NavLink}>
+            About
           </Nav.Link>
         </Nav>
         <Button
