fefc0ab280d74d9fc799975a194b3113812fcd56
techie-pro <techiepro6@gmail.com>
2022-08-18 07:22:55 +0530

    Final Implementations done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { About } from './pages/About';
+
 import { Store } from './pages/Store';
 import { Container } from 'react-bootstrap';
 import { Navbar } from './components/Navbar';
@@ -11,9 +10,7 @@ function App() {
       <Navbar />
       <Container className='mb-4'>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/store' element={<Store />}></Route>
-          <Route path='/about' element={<About />}></Route>
+          <Route path='/' element={<Store />}></Route>
         </Routes>
       </Container>
     </ShoppingCartProvider>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,14 +10,8 @@ export function Navbar() {
     <Navbarbs sticky='top' className='bg-white shadow-sm mb-3'>
       <Container>
         <Nav className='me-auto'>
-          <Nav.Link to='/' as={NavLink}>
-            Home
-          </Nav.Link>
           <Nav.Link to='/store' as={NavLink}>
             Store
-          </Nav.Link>
-          <Nav.Link to='/about' as={NavLink}>
-            About
           </Nav.Link>
         </Nav>
         {cartQuantity > 0 ? (
